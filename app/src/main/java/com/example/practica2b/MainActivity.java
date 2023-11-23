84de2287cddfe9b5f4e3d386600325ca9898e102
root <victormator5@gmail.com>
2023-11-23 18:27:05 +0100

    Proyecto a la mitad

---
+++ app/src/main/java/com/example/practica2b/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.practica2b;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_app_bar, menu);
+        return true;
+    }
+
+    public void loren(View view) {
+        Intent intent = new Intent(this, lorenActivity.class);
+        startActivity(intent);
+    }
+
+    public void salir(View view) {
+        System.exit(0);
+    }
+
+}
